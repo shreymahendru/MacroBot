7bba820e354c98b09d43e5dda62f8f131e929510
Shrey Mahendru <shreymahendru@gmail.com>
2016-12-25 20:07:38 -0500

    removed db ... trying to make heroku work

--- settings.py
+++ settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'MacroBot/db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'MacroBot/db.sqlite3'),
+#     }
+# }
 
 
 # Internationalization
